695f831d86e646466be9a08dcf20f83af0e2622a
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2023-04-19 14:01:10 +0200

    Added skeleton to save PNG images

---
+++ accumulator/src/write_png.c
@@ -0,0 +1,89 @@
+/*
+ * A simple libpng example program
+ * http://zarb.org/~gc/html/libpng.html
+ *
+ * Modified by Yoshimasa Niwa to make it much simpler
+ * and support all defined color_type.
+ *
+ * To build, use the next instruction on OS X.
+ * $ brew install libpng
+ * $ clang -lz -lpng16 libpng_test.c
+ *
+ * Copyright 2002-2010 Guillaume Cottenceau.
+ *
+ * This software may be freely redistributed under the terms
+ * of the X11 license.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <png.h>
+
+
+
+int write_png_file
+(char *filename,
+ png_bytep *row_pointers,
+ png_uint_32 width,
+ png_uint_32 height,
+ int depth,
+ int color_type)
+{
+  int y;
+
+  FILE *fp = fopen(filename, "wb");
+  if(!fp) { goto die; }
+
+  png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!png) { goto die; }
+
+  png_infop info = png_create_info_struct(png);
+  if (!info) { goto die; }
+
+  if (setjmp(png_jmpbuf(png))) { goto die; }
+
+  png_init_io(png, fp);
+
+  /* png_set_IHDR, (png_const_structrp png_ptr,
+     png_inforp info_ptr, png_uint_32 width, png_uint_32 height, int bit_depth,
+     int color_type, int interlace_method, int compression_method,
+     int filter_method
+  */
+
+  png_set_IHDR(
+	       png,
+	       info,
+	       width, height,
+	       depth,
+	       color_type,
+	       PNG_INTERLACE_NONE,
+	       PNG_COMPRESSION_TYPE_DEFAULT,
+	       PNG_FILTER_TYPE_DEFAULT
+	       );
+
+  png_write_info(png, info);
+
+  // To remove the alpha channel for PNG_COLOR_TYPE_RGB format,
+  // Use png_set_filler().
+  //png_set_filler(png, 0, PNG_FILLER_AFTER);
+
+  if (!row_pointers) { goto die; }
+
+  png_write_image(png, row_pointers);
+  png_write_end(png, NULL);
+
+  fclose(fp);
+
+  if (png && info)
+    { png_destroy_write_struct(&png, &info); }
+
+  return 0;
+
+ die:
+  if (png != NULL) { png_destroy_write_struct (&png, &info); }
+
+  if (fp != NULL) { fclose (fp); }
+
+  return -1;
+}

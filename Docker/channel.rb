34e1e37e2060965a80e3625e7b0766112d197258
Riccardo <framefritti@gmail.com>
2023-11-18 15:35:41 +0100

    Moved the communication details in a single file

---
+++ Docker/channel.rb
@@ -0,0 +1,39 @@
+require 'socket'
+
+Socket_Name = '/tmp/aaa'
+
+class Server_Side
+  @@listening_socket = nil
+  
+  def initialize
+    if @@listening_socket.nil?
+      File.unlink(Socket_Name) if File.exists?(Socket_Name)
+
+      @@listening_socket=UNIXServer.new(Socket_Name)
+    end
+
+    @to_client = @@listening_socket.accept
+  end
+
+  def readlines
+    @to_client.readlines
+  end
+
+  def close
+    @to_client.close
+  end
+end
+
+class Client_Side
+  def initialize
+    @to_server = UNIXSocket.new(Socket_Name)
+  end
+
+  def puts(x)
+    @to_server.puts(x)
+  end
+
+  def close
+    @to_server.close
+  end
+end

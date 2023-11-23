2245419d8eb0969bed989be5c1bace563dc14b11
Riccardo <framefritti@gmail.com>
2023-11-23 10:35:45 +0100

    Added check on git status

--- Docker/make-http-prod.sh
+++ Docker/make-http-prod.sh
@@ -3,4 +3,21 @@
 my_dir=`dirname $0`
 cd $my_dir
 
+function is_git_status_dirty() {
+    status=$(git status -s)
+    [ -n "$status" ]
+}
+
+
+if is_git_status_dirty ;  then
+    echo "CURRENT TREE IS NOT CLEAN. Exiting"
+    echo ---------------------------
+    echo
+    
+    git status
+
+    exit 1 
+fi
+
+
 sudo docker build -f ./Dockerfile.base-to-prod -t prod-httpd "$@" .

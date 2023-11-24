514e89f61ebb9ce021d3af85dc36da3c8e0cc5f5
Riccardo <framefritti@gmail.com>
2023-11-24 11:37:34 +0100

    Removed the use of 'let' (ma dimmi te...)

--- Docker/worker-manager.sh
+++ Docker/worker-manager.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/bash -eu
 
-source $HTTPD_PREFIX/library/dir_names.sh
+source /usr/local/apache2/library/dir_names.sh
 
 log_file=$log_dir/worker.log
 
 max_fast_restart=10
 
-let n_fast_restart=0
+n_fast_restart=0
 
 echo "$(date) MANAGER: Starting" >> $log_file
 
 while true ; do
-    let start=`date +%s`
+    start=`date +%s`
 
     echo "$(date) MANAGER: Starting worker" >> $log_file
 
     $bin_dir/worker.rb 2>> $log_file
 
-    let stop=`date +%s`
+    stop=`date +%s`
 
-    let delta=stop-start
+    delta=$((stop-start))
 
     if [ $delta -gt 300 ] ; then
-	let n_fast_restart=0
+	n_fast_restart=0
     else
-	let n_fast_restart++
+	n_fast_restart=$((n_fast_restart+1))
 
 	if [ $n_fast_restart -gt $max_fast_restart ]; then
 	    echo "$(date) MANAGER: Too many fast restarts. Giving up" >> $log_file

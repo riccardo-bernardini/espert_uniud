72f44a8397181fa8d9edfaf25c50414f3cb36775
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2024-08-23 14:57:30 +0200

    Made the branch used in the Dockerfile equal to the current branch
    (not always "master").  If the branch is not "master" the target name
    is test-httpd and not prod-httpd

--- Docker/make-http-prod.sh
+++ Docker/make-http-prod.sh
@@ -3,6 +3,13 @@
 my_dir=`dirname $0`
 cd $my_dir
 
+if [ 0 -gt 0 ]; then
+    branch=$1
+    shift
+else
+    branch=`git rev-parse --abbrev-ref HEAD`
+fi
+
 function is_git_status_dirty() {
     status=$(git status -s)
     [ -n "$status" ]
@@ -20,4 +27,15 @@ if is_git_status_dirty ;  then
 fi
 
 
-sudo docker build --no-cache -f ./Dockerfile.base-to-prod -t prod-httpd "$@" .
+if [ "$branch" = "master" ]; then
+    target=prod-httpd
+else
+    target=test-httpd
+fi
+
+sudo docker build \
+     --no-cache  \
+     -f ./Dockerfile.base-to-prod \
+     --build-arg "branch=$branch" \
+     -t "$target" \
+     "$@" .

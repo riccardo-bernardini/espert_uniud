a2e69ed605a63a70160f56e8afe2440654260617
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2024-08-29 11:19:17 +0200

    Improved the CLI handling
    - Added help
    - Added default when the output is a directory
    - Added checks on CLI arguments
    - Added check when operated as a filter, but stdin is a tty (likely the user did not want this)

--- spatial_filtering/dv_csv-to-aedat2.rb
+++ spatial_filtering/dv_csv-to-aedat2.rb
@@ -1,18 +1,81 @@
 #!/usr/bin/env ruby
 
-My_dir=File.dirname(File.expand_path($0))
-$LOAD_PATH.unshift(My_dir)
+require "pathname"
+
+Myself = Pathname.new($0)
+$LOAD_PATH.unshift(Myself.realdirpath.dirname)
 
 require "dv_aedat"
 require "dv_csv"
 
+Standard_Stream_Name = "-"
+
 class Config
+  def Config.basename(path)
+    if path == Standard_Stream_Name
+      return "stdin"
+    else
+      path = File.basename(path)
+      parts = path.split('.')
+
+      if parts.size == 1
+        return path
+      else
+        extension=parts[-1]
+        
+        return File.basename(path, ".#{extension}")
+      end
+    end
+  end
+
+  def Config.print_help
+    $stderr.puts "Usage: #{File.basename($0)} [input] [output]"
+    $stderr.puts
+    $stderr.puts "  input and output default to '-' (stdin or stdout)"
+    $stderr.puts "  If output is a directory, the filename default to"
+    $stderr.puts "  input with extension .aedat"
+    $stderr.puts
+  end
+  
   def Config.parse_command_line
-    @@filename_in  = ARGV.shift || "-"
-    @@filename_out = ARGV.shift || "-"
+    if ARGV.size > 0 && ["-h", "?", "--help"].include?(ARGV[0])
+      Config.print_help
+      exit 0
+    end
+    
+    @@filename_in  = ARGV.shift || Standard_Stream_Name
+    @@filename_out = ARGV.shift || Standard_Stream_Name
 
     Config.die("Junk at end of the line: #{ARGV.join(' ')}") unless
       ARGV.empty?
+
+    Config.die("'#{@@filename_in}' does not exist or it is not a file") unless
+      @@filename_in == Standard_Stream_Name ||
+      File.file?(@@filename_in)
+
+    if @@filename_out != Standard_Stream_Name
+      path=Pathname.new(@@filename_out)
+
+      if path.directory?
+        #
+        #  The second argument is a directory, use by default
+        #  the name of the input with the extension replaced by
+        #  aedat
+        #
+        filename = "#{Config.basename(@@filename_in)}.aedat"
+        @@filename_out = File.join(@@filename_out, filename)
+
+      elsif ! path.dirname.directory?
+        #
+        #  The second argument is not a directory which means that
+        #  (1) it exists and it is a file or (2) it does not exist.
+        #  In both cases, the part of path that precedes it must
+        #  be an existing directory.  If I am here, it is not.
+        #
+        Config.die("Bad output path #{@@filename_out}")
+
+      end
+    end
   end
 
   def Config.filename_in
@@ -57,12 +120,28 @@ class SmartFile < File
   end
 end
 
+###      ###
+### MAIN ###
+###      ###
+
 Config.parse_command_line
 
 SmartFile.open(Config.filename_in, :r) do |input|
   SmartFile.open(Config.filename_out, :w) do |output|
 
+    if input.tty?
+      $stderr.puts "Reading events from terminal"
+      $stderr.puts "If you did not want to do this, press Ctl-D to stop"
+      $stderr.puts "and print a help"
+      $stderr.puts
+    end
+    
     events, errors, metadata = DV::IO_CSV::read(input)
+
+    if events.empty? && input.tty?
+      Config.print_help
+      exit 0
+    end
 
     if Config.flip_x || Config.flip_y
       x_size = (metadata['sizeX'] || '640').to_i

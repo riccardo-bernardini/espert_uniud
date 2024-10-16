5405872b5bfe4717680a0b2411b8ff1707550f05
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2024-10-16 15:19:02 +0200

    Added another test

--- accumulator-de-noartri/src/test/dvaccum-filter_test.adb
+++ accumulator-de-noartri/src/test/dvaccum-filter_test.adb
@@ -1,10 +1,27 @@
 with Dvaccum.Filters;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Dvaccum.Filter_Test
 is
    use Filters;
 
-   F : constant Filter_Type := Parse("1.0 2.0 3.5 + 1.1 2.1 / 1.1 -1.0 + #1.9s", 1.0);
+   F : constant Filter_Type :=
+         Parse ("1.0 2.0 3.5 + 1.1 2.1 / 1.1 -1.0 + #1.9s", 1.0);
+
+   G : Filter_Type :=
+         Parse ("1.0 / 1.0 -0.5", 1.0);
+
+   X : constant Signal := (0 => 1.0, 1 => 0.0, 2 => 0.0, 3 => 0.0, 4 => 0.0);
+
+   Y : constant Signal := Apply (G, X);
 begin
-   Dump (F);
+   --  Dump (F);
+
+   Dump (G);
+
+   for C of Y loop
+      Put(C'Image & "  ");
+   end loop;
+
+   New_Line;
 end Dvaccum.Filter_Test;
